2c4fffe65bd606a09b76c83a26e8798642902e1d
alltradesjack <cc@nrdn.dk>
2020-08-25 23:12:32 +0200

    trying new authentication

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 from scrape import get_episode_playlist, get_newest_episode
 from spotipy_utils import get_track_ids, create_playlist_name, get_existing_playlists
 import os
+import configparser
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
+if os.name == 'nt':
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    client_id = config.get('SPOTIFY', 'CLIENT_ID')
+    client_secret = config.get('SPOTIFY', 'CLIENT_SECRET')
+else:
+    client_id = os.environ['CLIENT_ID']
+    client_secret = os.environ['CLIENT_SECRET']
+user = 'chrcarsten'
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="playlist-modify-public",
-                     client_id=str(os.environ.get('CLIENT_ID')),
-                     client_secret=str(os.environ.get('CLIENT_SECRET')),
-                     redirect_uri=str(os.environ.get('REDIRECT_URI')),
-                     username=str(os.environ.get('SPOTIFY_USER'))))
+auth = SpotifyOAuth(
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri='https://formiddag-med-mikael.herokuapp.com:8080',
+    username=user,
+    scope="playlist-modify-public"
+
+)
+
+token = auth.get_access_token()
+sp = spotipy.Spotify(auth=token['access_token'])
+
 
 episode_link = get_newest_episode()
 artists, tracks = get_episode_playlist(episode_link)
